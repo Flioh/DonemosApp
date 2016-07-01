8afb35e9a76c20cd5d1e1492e3a2cf55014f25dc
Sebastian Ferreras <sebastian.ferreras@gmail.com>
2016-07-02 00:29:06 +0200

    Se agrega servicio de autocomplete de google. Implementacion inicial del formulario de creacion de donaciones.

--- app/pages/nueva-solicitud/nueva-solicitud.ts
+++ app/pages/nueva-solicitud/nueva-solicitud.ts
@@ -1,36 +1,74 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Control, Validators, AbstractControl } from '@angular/common';
+
+import {AutocompleteService} from '../../providers/autocomplete-service/autocomplete-service';
+import {ConnectivityService} from '../../providers/connectivity-service/connectivity-service';
 
 /* Models */
 import {NuevaSolicitudModel} from '../../providers/nueva-solicitud-model/nueva-solicitud-model';
 
 @Component({
   templateUrl: 'build/pages/nueva-solicitud/nueva-solicitud.html',
-  directives: [FORM_DIRECTIVES]
+  directives: [FORM_DIRECTIVES],
+  providers: [AutocompleteService]
 })
 export class NuevaSolicitudPage {
 
 	private ocultarIconoNuevaSolicitud: boolean = true;
-	nuevaSolicitud: NuevaSolicitudModel;
-	
-	nuevaSolicitudForm: ControlGroup;
+
+	private nuevaSolicitud: NuevaSolicitudModel;
+	private nuevaSolicitudForm: ControlGroup;
+
+	// Informacion de Google maps
+	apiKey: any;
 
 	// Campos del formulario
-	private lugar: Control;
+	private ctrlInstitucion: Control;
+	private institucionValue: string = '';
+	
+	private ctrlDireccion: Control;
+	private direccionValue: string = '';
 
+	private elementId: string = 'autocomplete';
 
-	constructor(private nav: NavController, private formBuilder : FormBuilder) {
+	constructor(private nav: NavController, 
+				private formBuilder : FormBuilder, 
+				private ngZone : NgZone,
+				private connectivityService : ConnectivityService,
+				private autocompleteService : AutocompleteService) {
 		
-		this.lugar = new Control('', Validators.required);
+		this.ctrlInstitucion = new Control(this.institucionValue, Validators.required);
+		this.ctrlDireccion = new Control(this.direccionValue, Validators.required);
+
 
 		this.nuevaSolicitudForm = formBuilder.group({
-			lugar: this.lugar
+			institucion: this.ctrlInstitucion,
+			direccion: this.ctrlDireccion
+		});
+
+		// Nos suscribimos al autocomplete para que nos envie la informacion de la direccion
+		this.autocompleteService.autocomplete.subscribe((informacionDelLugar) => {
+			this.ngZone.run(() => {
+				this.setearDireccion(informacionDelLugar);
+			});
 		});
 	}
 
-	ionViewLoaded() {
-	  // Antes de ingresar, verificar si hay información no guardada
+	setearDireccion(informacionSobreDireccion: any) {
+		debugger;
+		this.institucionValue = informacionSobreDireccion.getName();
+		this.ctrlInstitucion.updateValueAndValidity(this.institucionValue)
+
+		this.direccionValue = informacionSobreDireccion.getAdressLine1();
+		this.ctrlDireccion.updateValueAndValidity(this.direccionValue);
+	}
+
+	ionViewDidEnter() {
+	  	// Antes de ingresar, verificar si hay información no guardada
+
+	  	// Inicializar el autocomplete
+		this.autocompleteService.initializeAutocomplete(document.getElementById('autocomplete').childNodes[0]);
 	}
 
 	ionViewWillLeave() {
@@ -44,4 +82,8 @@ export class NuevaSolicitudPage {
 	public guardarCambios(): void {
 
 	}
+
+	get cgValue(): string {
+    	return JSON.stringify(this.nuevaSolicitudForm.value, null, 2);
+  	}
 }
---
+++ app/providers/autocomplete-service/autocomplete-service.ts
@@ -0,0 +1,135 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import {ConnectivityService} from '../../providers/connectivity-service/connectivity-service';
+
+@Injectable()
+export class AutocompleteService {
+
+	private autocompleteObserver: any;
+	public autocomplete: any;
+
+	private apiKey: string;
+
+	private placeSearch: any;
+	private autocompleteElement: any;
+	private googleAddressInformation: any = {};
+
+	constructor(private connectivityService: ConnectivityService) {
+		this.autocompleteObserver = null;
+
+		this.autocomplete = Observable.create(observer => {
+			this.autocompleteObserver = observer;
+		});
+
+		// Load the Google Places library
+		if(this.connectivityService.isOnline()) {
+			this.loadGoogleMapsLibrary();
+		}
+	}
+
+	private loadGoogleMapsLibrary(): void {
+		if (typeof google == "undefined" || typeof google.maps == "undefined") {
+
+			let script = document.createElement("script");
+			script.id = "googleMaps";
+
+			if (this.apiKey) {
+				script.src = 'http://maps.googleapis.com/maps/api/js?key=' + this.apiKey + '&libraries=places';
+			} else {
+				script.src = 'http://maps.googleapis.com/maps/api/js?libraries=places';
+			}
+			document.body.appendChild(script);
+		}
+	}
+
+	public initializeAutocomplete(element): void {
+		// Create the autocomplete object, restricting the search to geographical location types.
+		this.autocompleteElement = new google.maps.places.Autocomplete(element);
+
+		window.processAddress = () => {
+			this.processAddressInformation();
+		}
+
+		// When the user selects an address from the dropdown,
+		// populate the address fields in the form.
+		google.maps.event.addListener(this.autocompleteElement, 'place_changed', function() {
+			processAddress();
+		});
+	}
+
+	public processAddressInformation(): void {
+		// Get the place details from the autocomplete object.
+		let place = this.autocompleteElement.getPlace();
+
+		// Clean previous information
+		this.googleAddressInformation = {};
+
+		for (let i = 0; i < place.address_components.length; i++) {
+			let addressType = place.address_components[i].types[0];
+			let attributeToGet;
+			if (addressType === 'country') {
+				attributeToGet = 'short_name';
+			}
+			else {
+				attributeToGet = 'long_name';
+			}
+
+			this.googleAddressInformation[addressType] = place.address_components[i][attributeToGet];
+		}
+
+		// Aditional properties
+		this.googleAddressInformation['name'] = place['name'];
+		this.googleAddressInformation['place_id'] = place['place_id'];
+		this.googleAddressInformation['formatted_phone_number'] = place['formatted_phone_number'];
+
+		this.returnAddress();
+	}
+
+	private returnAddress(): void {
+		let streetNumber = this.googleAddressInformation.street_number ? this.googleAddressInformation.street_number : '',
+		route = this.googleAddressInformation.route ? this.googleAddressInformation.route : '',
+		city = this.googleAddressInformation.locality ? this.googleAddressInformation.locality : '',
+		country = this.googleAddressInformation.country ? this.googleAddressInformation.country : '',
+		province = this.googleAddressInformation.administrative_area_level_1 ? this.googleAddressInformation.administrative_area_level_1 : '',
+		postal_code = this.googleAddressInformation.postal_code ? this.googleAddressInformation.postal_code : '',
+		postal_code_suffix = this.googleAddressInformation.postal_code_suffix ? this.googleAddressInformation.postal_code_suffix : '',
+		name = this.googleAddressInformation.name ? this.googleAddressInformation.name : '',
+		place = this.googleAddressInformation.place_id ? this.googleAddressInformation.place_id : '',
+		phoneNumber = this.googleAddressInformation.formatted_phone_number ? this.googleAddressInformation.formatted_phone_number : '';
+
+		let result = {
+
+			getName : function(): string {
+				return name;
+			},
+			getPlace : function(): string {
+				return place;
+			},
+			getPhoneNumber : function(): string {
+				return phoneNumber;
+			},
+			getAdressLine1: function(): string {
+				return route + ' ' + streetNumber;
+			},
+			getCity: function(): string {
+				return city;
+			},
+			getProvince: function(): string {
+				return province;
+			},
+			getCountryCode: function(): string {
+				return country;
+			},
+			getPostalCode: function(): string {
+				return postal_code + postal_code_suffix;
+			},
+			getEntireResponse: function(): any {
+				return this.googleAddressInformation;
+			}
+		}
+
+		// Send informtaion back to the parent directive
+		this.autocompleteObserver.next(result);
+	}
+}
