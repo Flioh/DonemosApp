8cb342585097457106348fa51dce2c3e79a736c4
Sebastian Ferreras <sebastian@flioh.com>
2016-09-02 09:35:20 +0200

    Se agrega pantalla con los detalles de la solicitud.

--- app/pages/detalles-solicitud/detalles-solicitud.ts
+++ app/pages/detalles-solicitud/detalles-solicitud.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams, Alert} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, NavParams, Alert } from 'ionic-angular';
 import { NuevaSolicitudPage } from '../nueva-solicitud/nueva-solicitud';
 
 @Component({
 	/* This should not be done anymore */
-	templateUrl: 'build/pages/detalles-solicitud/detalles-solicitud.html',
+	templateUrl: 'build/pages/detalles-solicitud/detalles-solicitud.html'
 })
 export class DetallesSolicitudPage {
 
@@ -15,6 +15,27 @@ export class DetallesSolicitudPage {
   		// Obtenemos la solicitud seleccionada a traves de navParams
   		this.solicitudSeleccionada = navParams.get('unaSolicitud');
   	}
+
+  	ionViewDidEnter(){
+        let latLng = new google.maps.LatLng(43.458937999999996, -3.820382);               
+
+        let mapOptions = {
+            center: latLng,
+            zoom: 15,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        let marker = new google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
+            position: map.getCenter()
+        });
+
+
+    }
 
   	nuevaSolicitud(): void {
 		this.nav.setRoot(NuevaSolicitudPage, {}, { animate: true, direction: 'forward' });
