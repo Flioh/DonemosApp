0e8f86a82f04329d91a56fec5e0392644bf43f90
Sebastian Ferreras <sebastian.ferreras@gmail.com>
2016-09-11 22:56:36 +0200

    Se elimina el pipe de tipos sanguineos. Se utilizan los datos del usuario para resaltar los tipos sanguineos en las solicitudes.

--- app/directives/solicitud-item/solicitud-item.ts
+++ app/directives/solicitud-item/solicitud-item.ts
@@ -1,36 +1,55 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { SolicitudModel } from '../../providers/solicitud-model/solicitud-model';
 import { DonacionesHelper } from '../../providers/donemos-helper-service/donemos-helper-service';
-import { TiposSanguineosPipe } from '../../pipes/format-tipos-sanguineos/format-tipos-sanguineos-pipe';
 import { TimeAgoPipe } from '../../pipes/time-ago/time-ago-pipe';
+import { SqlStorage, Storage } from 'ionic-angular';
 
 @Component({
     selector: 'solicitud-item',
     templateUrl: 'build/directives/solicitud-item/solicitud-item.html',
-    pipes: [ TiposSanguineosPipe, TimeAgoPipe]
+    pipes: [ TimeAgoPipe ]
 })
 export class SolicitudItem {
-	@Input() solicitud: SolicitudModel;
-	@Output() seleccionarSolicitud = new EventEmitter();
+	  @Input() solicitud: SolicitudModel;
+	  @Output() seleccionarSolicitud = new EventEmitter();
 
-    constructor() { }
+    private storage : Storage;
+    private datosUsuario : string;
+
+    constructor() {
+
+      this.datosUsuario = '';
+
+      if(!this.storage) {
+        this.storage = new Storage(SqlStorage);  
+      }
+
+      this.storage.get('datosUsuarioObj').then((datosUsuario) => {
+        if(datosUsuario) {
+          let datosUsuarioObj = JSON.parse(datosUsuario);
+          this.datosUsuario = DonacionesHelper.getDescripcion(datosUsuarioObj.grupoSanguineoID, datosUsuarioObj.factorSanguineoID);
+        }
+      });
+    }
 
     public verDetalles(): void {
     	this.seleccionarSolicitud.emit({ value : this.solicitud });
     }
 
     // Obtiene el listado de grupos y factores que se necesitan
-    public getTiposSanguineosBuscados(): Array<string> {
-      return DonacionesHelper.puedeRecibirDe(this.solicitud.getGrupoSanguineo().getId(), 
+    public mostrarTiposBuscados(): string {
+      let result = '';
+      let posiblesDadores = DonacionesHelper.puedeRecibirDe(this.solicitud.getGrupoSanguineo().getId(), 
                                              this.solicitud.getFactorSanguineo().getId());
+
+      for(let i=0; i<posiblesDadores.length; i++) {
+        if(posiblesDadores[i] == this.datosUsuario) {
+          result += '<span class="marked">' + posiblesDadores[i] + '</span> ';
+        } else {
+          result += posiblesDadores[i] + ' ';
+        }
+      }
+
+      return result;
     }
-
-    // Obtiene el factor y grupo sanguineo del usuario
-    public getDescripcionTipoSanguineo():string {
-      // TODO: reemplazar por los datos del usuario si existen
-      // -----------------------------------------------------
-      return '';
-    }
-
-
 }
--- app/pages/detalles-solicitud/detalles-solicitud.ts
+++ app/pages/detalles-solicitud/detalles-solicitud.ts
@@ -1,24 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, SqlStorage, Storage } from 'ionic-angular';
 import { NuevaSolicitudPage } from '../nueva-solicitud/nueva-solicitud';
 import { DonacionesHelper } from '../../providers/donemos-helper-service/donemos-helper-service';
-import { TiposSanguineosPipe } from '../../pipes/format-tipos-sanguineos/format-tipos-sanguineos-pipe';
 import { TimeAgoPipe } from '../../pipes/time-ago/time-ago-pipe';
 import { SolicitudModel } from '../../providers/solicitud-model/solicitud-model';
 
 @Component({
 	templateUrl: 'build/pages/detalles-solicitud/detalles-solicitud.html',
-  pipes: [ TiposSanguineosPipe, TimeAgoPipe]
+  pipes: [ TimeAgoPipe ]
 })
 export class DetallesSolicitudPage {
 
 	// Variables de la clase
 	private solicitudSeleccionada: SolicitudModel;
 
+  private datosUsuario: string;
+  private compatibleConUsuario: boolean;
+
+  private storage: Storage;
+
 	constructor(private nav: NavController, navParams: NavParams, private loadingCtrl: LoadingController ) {
   		// Obtenemos la solicitud seleccionada a traves de navParams
   		this.solicitudSeleccionada = navParams.get('unaSolicitud');
+
+      this.obtenerDatosUsuario();
   	}
+
+    public obtenerDatosUsuario(){
+      this.datosUsuario = '';
+
+      if(!this.storage) {
+        this.storage = new Storage(SqlStorage);  
+      }
+
+      this.storage.get('datosUsuarioObj').then((datosUsuario) => {
+        if(datosUsuario) {
+          let datosUsuarioObj = JSON.parse(datosUsuario);
+          this.datosUsuario = DonacionesHelper.getDescripcion(datosUsuarioObj.grupoSanguineoID, datosUsuarioObj.factorSanguineoID);
+        }
+      });
+    }
 
     // Inicializa el mapa cuando el DOM ya esta listo
   	ionViewDidEnter(){
@@ -53,6 +74,26 @@ export class DetallesSolicitudPage {
             //valert('Geocode was not successful for the following reason: ' + status);
           }
         });
+    }
+
+    // Obtiene el listado de grupos y factores que se necesitan
+    public mostrarTiposBuscados(): string {
+      let result = '';
+      this.compatibleConUsuario = false;
+
+      let posiblesDadores = DonacionesHelper.puedeRecibirDe(this.solicitudSeleccionada.getGrupoSanguineo().getId(), 
+                                             this.solicitudSeleccionada.getFactorSanguineo().getId());
+
+      for(let i=0; i<posiblesDadores.length; i++) {
+        if(posiblesDadores[i] == this.datosUsuario) {
+          result += '<span class="marked">' + posiblesDadores[i] + '</span> ';
+          this.compatibleConUsuario = true;
+        } else {
+          result += posiblesDadores[i] + ' ';
+        }
+      }
+
+      return result;
     }
 
     // Obtiene el listado de grupos y factores que se necesitan
