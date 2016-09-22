0c1dd76e75535225134915fc7b1044ae21cfa28d
Sebastian Ferreras <sebastian.ferreras@gmail.com>
2016-09-22 14:59:13 +0200

    Se reemplaza el mapa por una imagen estatica.

--- app/app-config.ts
+++ app/app-config.ts
@@ -4,13 +4,17 @@ export interface ApplicationConfig {
   apiEndPointProvincias: string;
   apiEndPointLocalidades: string;
   apiEndPointSolicitudes: string;
+  staticMapUrl: string;
+  staticMapKey: string;
 }
 
 // Objeto de configuracion para la aplicacion
 export const MY_CONFIG: ApplicationConfig = {
-  apiEndPointProvincias: 	'./provincias.json',
+  apiEndPointProvincias: 	  './provincias.json',
   apiEndPointLocalidades: 	'./localidades.json',
   apiEndPointSolicitudes: 	'./solicitudes.json',
+  staticMapUrl:             'https://maps.googleapis.com/maps/api/staticmap',
+  staticMapKey:             ''
 };
 
 // Creamos un token para evitar conflictos por nombres
--- app/pages/detalles-solicitud/detalles-solicitud.ts
+++ app/pages/detalles-solicitud/detalles-solicitud.ts
@@ -1,10 +1,12 @@
+import { ConfigMock } from '../../../test/mocks';
 import { TimeAgoPipe } from '../../pipes/time-ago/time-ago-pipe';
 import { DonacionesHelper } from '../../providers/donemos-helper-service/donemos-helper-service';
 import { RemoteDataService } from '../../providers/remote-data-service/remote-data-service';
 import { SolicitudModel } from '../../providers/solicitud-model/solicitud-model';
 import { UserDataService } from '../../providers/user-data-service/user-data-service';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { LoadingController, NavController, NavParams } from 'ionic-angular';
+import { MY_CONFIG, MY_CONFIG_TOKEN, ApplicationConfig } from '../../app-config.ts';
 
 @Component({
 	templateUrl: 'build/pages/detalles-solicitud/detalles-solicitud.html',
@@ -15,6 +17,9 @@ export class DetallesSolicitudPage {
 	// Variables de la clase
 	private solicitudSeleccionada: SolicitudModel;
 
+  // Informacion del mapa
+  private mapSrc: string;
+
   private tiposSanguineosSolicitud: string;
   private compatibleConUsuario: boolean;
 
@@ -24,8 +29,9 @@ export class DetallesSolicitudPage {
               private navParams: NavParams, 
               private loadingCtrl: LoadingController, 
               private remoteDataService: RemoteDataService,
-              private userDataService: UserDataService) {
-
+              private userDataService: UserDataService,
+              @Inject(MY_CONFIG_TOKEN) config: ApplicationConfig) {
+      
       if(this.remoteDataService.modoDebugActivado()) {
         console.time('DetallesSolicitudPage / constructor');
       }
@@ -35,37 +41,22 @@ export class DetallesSolicitudPage {
 
       this.obtenerDatosUsuario();
 
+      // Crea el mapa
+      this.obteneMapaUrl(config.staticMapUrl, config.staticMapKey, 400, 400, 16);
+
       if(this.remoteDataService.modoDebugActivado()) {
         console.timeEnd('DetallesSolicitudPage / constructor');
       }
   	}
 
-    public obtenerDatosUsuario(){
+    // Método que crea la imagen del mapa
+    public obteneMapaUrl(mapUrl: string, mapKey: string, width: number, height: number, zoom: number): void {
+    
+      // Obtenemos la direccion de la institución
+      let direccion = `${ this.solicitudSeleccionada.getDireccion() },${ this.solicitudSeleccionada.getCiudad().getNombre() },${ this.solicitudSeleccionada.getProvincia().getNombre() },Argentina`;
 
-      if(this.remoteDataService.modoDebugActivado()) {
-        console.time('DetallesSolicitudPage / obtenerDatosUsuario');
-      }
-
-      this.tiposSanguineosSolicitud = DonacionesHelper.puedeRecibirDe(this.solicitudSeleccionada.getGrupoSanguineo().getId(), 
-                                                                      this.solicitudSeleccionada.getFactorSanguineo().getId()).join(' ');
-      this.compatibleConUsuario = false;
-
-      // Obtenemos los datos del servicio de datos del usuario
-      this.userDataService.getDatosUsuario().then((datosUsuario) => {
-        if(datosUsuario) {
-          let tipoSanguineoUsuario = DonacionesHelper.getDescripcion(datosUsuario.grupoSanguineoID, datosUsuario.factorSanguineoID);
-
-          // Si es compatible mostramos un mensjae informandolo
-          this.compatibleConUsuario = this.tiposSanguineosSolicitud.indexOf(tipoSanguineoUsuario) > -1;
-
-          // Resaltamos el tipo sanguineo del usuario
-          this.tiposSanguineosSolicitud = this.tiposSanguineosSolicitud.replace(tipoSanguineoUsuario, '<span class="marked">' + tipoSanguineoUsuario + '</span> ');
-
-          if(this.remoteDataService.modoDebugActivado()) {
-            console.timeEnd('DetallesSolicitudPage / obtenerDatosUsuario');
-          }
-        }
-      });
+      // Generamos la url del mapa estatico
+      this.mapSrc = `${ mapUrl }?zoom=${ zoom }&size=${ width }x${ height }&markers=color:blue%7C${ direccion.replace(/ /g, '+') }&key=${ mapKey }`;
     }
 
     // Inicializa el mapa cuando el DOM ya esta listo
@@ -109,6 +100,36 @@ export class DetallesSolicitudPage {
           //valert('Geocode was not successful for the following reason: ' + status);
         }
       });*/
+    }
+
+
+    // Método que obtiene los datos del usuario
+    public obtenerDatosUsuario(){
+
+      if(this.remoteDataService.modoDebugActivado()) {
+        console.time('DetallesSolicitudPage / obtenerDatosUsuario');
+      }
+
+      this.tiposSanguineosSolicitud = DonacionesHelper.puedeRecibirDe(this.solicitudSeleccionada.getGrupoSanguineo().getId(), 
+                                                                      this.solicitudSeleccionada.getFactorSanguineo().getId()).join(' ');
+      this.compatibleConUsuario = false;
+
+      // Obtenemos los datos del servicio de datos del usuario
+      this.userDataService.getDatosUsuario().then((datosUsuario) => {
+        if(datosUsuario) {
+          let tipoSanguineoUsuario = DonacionesHelper.getDescripcion(datosUsuario.grupoSanguineoID, datosUsuario.factorSanguineoID);
+
+          // Si es compatible mostramos un mensjae informandolo
+          this.compatibleConUsuario = this.tiposSanguineosSolicitud.indexOf(tipoSanguineoUsuario) > -1;
+
+          // Resaltamos el tipo sanguineo del usuario
+          this.tiposSanguineosSolicitud = this.tiposSanguineosSolicitud.replace(tipoSanguineoUsuario, '<span class="marked">' + tipoSanguineoUsuario + '</span> ');
+
+          if(this.remoteDataService.modoDebugActivado()) {
+            console.timeEnd('DetallesSolicitudPage / obtenerDatosUsuario');
+          }
+        }
+      });
     }
   }
 
