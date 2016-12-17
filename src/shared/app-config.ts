b9277d373d4b810ed2da601846b9e8e2b7a81e66
Sebastian Ferreras <sebastian.ferreras@gmail.com>
2016-12-17 22:30:17 +0100

    Se crean archivos separados para las key de desarrollo y de producción

--- src/shared/app-config.ts
+++ src/shared/app-config.ts
@@ -1,36 +1,61 @@
 // Referencias de Angular
 import { Injectable } from '@angular/core';
 
+// Keys
+import { DevKeys } from './dev-keys';
+import { ProdKeys } from './prod-keys';
+
 // Interface utilizada para evitar warnings de Typescript
 export interface ApplicationConfig {
-  apiEndPointProvincias: string;
-  apiEndPointLocalidades: string;
-  apiEndPointSolicitudes: string;
-  staticMapUrl: string;
-  staticMapKey: string;
+    apiEndPointProvincias: string;
+    apiEndPointLocalidades: string;
+    apiEndPointSolicitudes: string;
+    staticMapUrl: string;
+    staticMapKey: string;
 }
 
 @Injectable()
 export class AppConfig {
 
-  // Endpoints
-  public apiEndPointProvincias: string    =	          'https://d70bcd30.ngrok.io/provincia';
-  public apiEndPointLocalidades: string   = 	        'https://d70bcd30.ngrok.io/localidad';
-  public apiEndPointSolicitudes: string   =	          'https://d70bcd30.ngrok.io/solicitud';
-  public apiEndPointBancosSangre: string  =           'https://d70bcd30.ngrok.io/banco';
+    public modoDebug: boolean = true;    
+    private apiVersion: number = 1;
 
-  // Google maps
-  public staticMapUrl: string =                       'https://maps.googleapis.com/maps/api/staticmap';
-  public autocompleteKey: string =                    '';
-  public staticMapKey: string =                       '';
-  public staticMapTamanio: number =                   400;
-  public staticMapZoom: number =                      16;
+    // Google maps
+    public staticMapUrl: string = 'https://maps.googleapis.com/maps/api/staticmap';
+    public staticMapTamanio: number = 400;
+    public staticMapZoom: number = 16;
 
-  // Auth0
-  public authClientId: string =                       '';
-  public authDomain: string =                         '';
+    // Endpoints
+    // -----------------------------------
+    get apiEndPointProvincias(): string {
+        return this.modoDebug ? `${DevKeys.apiUrl}/provincia` : `${ProdKeys.apiUrl}/${this.apiVersion}/provincia`;
+    }
 
-  // Otras configuraciones
-  public bugSnagApiKey: string =                      '';
-  public modoDebug: boolean =                         false;
+    get apiEndPointLocalidades(): string {
+        return this.modoDebug ? `${DevKeys.apiUrl}/localidad` : `${ProdKeys.apiUrl}/${this.apiVersion}/localidad`;
+    }
+
+    get apiEndPointSolicitudes(): string {
+        return this.modoDebug ? `${DevKeys.apiUrl}/solicitud` : `${ProdKeys.apiUrl}/${this.apiVersion}/solicitud`;
+    }
+
+    get apiEndPointBancosSangre(): string {
+        return this.modoDebug ? `${DevKeys.apiUrl}/banco` : `${ProdKeys.apiUrl}/${this.apiVersion}/banco`;
+    }
+
+    // Google maps
+    // -----------------------------------
+    get autocompleteKey(): string {
+        return this.modoDebug ? DevKeys.autocompleteKey : ProdKeys.autocompleteKey;
+    }
+
+    get staticMapKey(): string {
+        return this.modoDebug ? DevKeys.staticMapKey : ProdKeys.staticMapKey;
+    }
+
+    // Bugsnag
+    // -----------------------------------
+    get bugSnagApiKey(): string {
+        return this.modoDebug ? DevKeys.bugSnagApiKey : ProdKeys.bugSnagApiKey;
+    }
 }
---
+++ src/shared/prod-keys.ts
@@ -0,0 +1,17 @@
+export class ProdKeys {
+
+    // Api
+    static apiUrl: string = '';
+
+    // Google maps
+    static autocompleteKey: string = '';
+    static staticMapKey: string = '';
+
+    // Auth0
+    static authClientId: string = '';
+    static authDomain: string = '';
+
+    // Otras configuraciones
+    static bugSnagApiKey: string = '';
+    
+}
